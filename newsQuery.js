6ab5d4f95080f127eba0487d84044baf0eb77455
abx393 <abandari99@gmail.com>
2020-10-17 16:30:05 -0700

    fixed news query api

--- newsQuery.js
+++ newsQuery.js
@@ -1,36 +1,34 @@
-// News API Queries
-
-const proxy = "https://cors-anywhere.herokuapp.com/";
-const apiKey = "4f010001c9f14c2eb9288c298c164b07"
+const apiKey = "edb5995c5150404fbc55e6530b36cddc";
 
 // @param keywords: list of keywords
 // @param domains: A comma-seperated string of domains (eg bbc.co.uk, techcrunch.com, engadget.com) to restrict the search to.
-// @param from: timestamp (yyyy-mm-dd)
-// @param sortBy: one of "relevancy", "popularity", "publishedAt"
-function queryNews(keywords, domains, from, sortBy) {
+async function queryNews(keywords, domains) {
+    const sortBy = "relevancy"; // other possible values: "popularity", "publishedAt"
     const language = "en"; // searching for only English articles
+    const from = "2020-09-016"; // default start date of articles
+    
     var response;
     
     var keywordsQuery = "q=" + keywords[0];
     for (let i = 1; i < keywords.length; i++) {
-        keywordsQuery += " OR " + keywords[i];
+        keywordsQuery += "OR" + keywords[i];
     }
     
-    var url = "http://newsapi.org/v2/everything?" +
-              keywordsQuery + "&" +
-              "domains=" + domains + "&" +
-              "from=" + from + "&" +
-              "language=" + language + "&" +
-              "sortBy=" + sortBy + "&" +
-              "apiKey=" + apiKey;
+    console.log("apiKey " + apiKey);
     
-    var request = new Request(proxy + url);
-
-    fetch(request)
-        .then(response => response.json())
-        .then(function(data) {
-            response = data;
-    });
+    console.log("keyWordsQuery " + keywordsQuery);
+    keywordsQuery = encodeURIComponent(keywordsQuery);
+    console.log("keyWordsQuery " + keywordsQuery);
     
-    return response;
+    var url = "http://newsapi.org/v2/everything?" + keywordsQuery + 
+              "&domains=" + domains +
+              "&from=" + from +
+              "&language=" + language +
+              "&sortBy=" + sortBy +
+              "&apiKey=" + apiKey;
+    console.log("url " + url);
+    
+    var request = new Request(url);
+    var response = await fetch(request);
+    return response.json();
 }
