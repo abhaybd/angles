2b417d84abe3f57c551bd036e73291115999cbd1
Ian Wang <35878784+iwangy@users.noreply.github.com>
2020-10-17 14:45:31 -0700

    add frontend button window

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
 chrome.runtime.onInstalled.addListener(function() {
-    console.log("Installed!");
-})
+    chrome.storage.sync.set({color: '#3aa757'}, function(){
+        console.log("The color is green.");
+    });
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+        chrome.declarativeContent.onPageChanged.addRules([{
+          conditions: [new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {schemes:['https']},
+          })
+          ],
+              actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+      });
+});
--- content.js
+++ content.js
@@ -1,3 +1,21 @@
+//TEST CODE FOR IAN TO REFERENCE
+let changeColor = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', function(data) {
+  changeColor.style.backgroundColor = data.color;
+  changeColor.setAttribute('value', data.color);
+});
+
+changeColor.onclick = function(element) {
+    let color = element.target.value;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    });
+  };
+
+
 function isNews(origin) {
     // TODO: determine if origin is a news site
     return true;
