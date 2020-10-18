ee7a0685824170d2f370ee3e56b9d2db6d7c9419
Ian Wang <35878784+iwangy@users.noreply.github.com>
2020-10-17 19:16:05 -0700

    text wrap in popup

--- background.js
+++ background.js
@@ -1,20 +1,13 @@
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(async function() {
     console.log('check');
     var keywords = ["president", "election"];
     var domains = "cnn.com,foxnews.com";
-    var queryResults = queryNews(keywords, domains);
+    var queryResults = await queryNews(keywords, domains);
     console.log("queryResults " + queryResults);
 });
 
-function getRelevantNews(keywords, publisher) {
-    const bias = getBias(publisher);
-    const publishers = oppositeBias(bias);
-    // TODO: invoke news api with publishers and keywords
-    return null;
-}
-
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
+    async function(request, sender, sendResponse) {
         console.log("From tab: " + sender.tab.url);
         switch (request.reqType) {
             case "publisher":
@@ -22,7 +15,7 @@ chrome.runtime.onMessage.addListener(
                 break;
 
             case "news":
-                const news = getRelevantNews(request.keywords, request.publisher);
+                const news = await getRelevantNews(request.keywords, request.publisher);
                 sendResponse({articles: news});
                 break;
 
@@ -41,3 +34,41 @@ chrome.runtime.onMessage.addListener(
         }
     }
 );
+
+async function getRelevantNews(keywords, publisher) {
+    const bias = getBias(publisher);
+    const publishers = oppositeBias(bias);
+    
+    // domains must be comma-separated list of domain names
+    var domains = publishers[0][1];
+    for (let i = 1; i < publishers.length; i++) {
+        domains += "," + publishers[i][1];
+    }
+    console.log("publishers " + publishers);
+    
+    var oppositeBiases = [];
+    for (let i = 0; i < publishers.length; i++) {
+        oppositeBiases[i].push(publishers[i][2]);
+    }
+    
+    console.log("domains " + domains);
+    
+    var response = await queryNews(keywords, domains);
+    
+    var numArticles = response.totalResults;
+    var articles = response.articles;
+    
+    for (let i = 0; i < numArticles; i++) {
+        var currentPublisher = articles[i].source.name;
+        
+        //find bias of currentPublisher and set that as current article's bias
+        for (let j = 0; j < publishers.length; j++) {
+            if (publishers[j][0].equals(currentPublisher)) {
+                articles[i]["bias"] = oppositeBiases[j];
+                break;
+            }
+        }
+    }
+    
+    return articles;
+}
