df75f87e2b51837457cc323aab6c162a03d78af9
arnavthareja <arnav.thareja25@gmail.com>
2020-10-17 19:22:34 -0700

    Finish keyword extraction cloud function

---
+++ cloud_functions/keyword.js
@@ -0,0 +1,78 @@
+// Host in Google Cloud function with node and @google-cloud/language installed
+// Get all Entities in articleText and return entities with highest salience values
+// Access URL: https://us-west3-angles-dh2020.cloudfunctions.net/getRelevantEntities
+
+const language = require('@google-cloud/language');
+
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.getRelevantEntities = (req, res) => {
+  let text;
+  switch (req.get('content-type')) {
+    case 'application/json':
+      text = req.body.message;
+      break;
+
+    case 'text/plain':
+      text = req.body;
+      break;
+
+    case 'application/octet-stream':
+      text = req.body.toString();
+      break;
+
+    case 'application/x-www-form-urlencoded':
+      text = req.body.message;
+      break;
+  }
+  getEntities(text).then(function(keywords) {
+    res.status(200).send(keywords);
+  }).catch(function(error) {
+    res.status(200).send('error: ' + error);
+  });
+};
+
+async function getEntities(articleText, n=5) {  
+  // Instantiates a client
+  const client = new language.LanguageServiceClient();
+
+  const document = {
+    content: articleText,
+    type: 'PLAIN_TEXT',
+  };
+
+  // Detects the sentiment of the text
+  const [result] = await client.analyzeEntities({document});
+  var entities = result.entities;
+
+  // Sort entities by salience
+  entities.sort((a, b) => b.salience - a.salience);
+
+  // Return top n entities (5 default)
+  // If n > entities.length, return entities
+  let keywords = [];
+  /*
+  for (i = 0; i < n; i++) {
+    if (n === entities.length) break;
+    let entity;
+    try {
+      entity = JSON.parse(entities[i]);
+    } catch (e) {
+      entity = entities[i];
+    }
+    keywords.push(entity.name);
+  }
+  */
+  let i = 0;
+  entities.forEach(entity => {
+    if (i < n) {
+      keywords.push(entity.name);
+      i++;
+    }
+  });
+  return JSON.stringify({"keywords": keywords});
+}
