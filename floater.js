8ccde373e38c36d476802ee244abb0aaf63fdb00
Abhay D <a.deshpande012@gmail.com>
2020-10-18 01:06:36 -0700

    Fixes to slider

--- floater.js
+++ floater.js
@@ -34,19 +34,19 @@ function populateFloater(floater, publisher) {
     getKeywords().then(keywords => {
         relevantNews(keywords, publisher, response => {
             floater.find(".current-article").text(document.title);
-            floater.find("#myRange").value = (response.currBias - 1) * 25;
+            let value = (response.currBias - 1) * 25;
+            document.getElementById("source-slider").value = value;
+            // $("#source-slider").slider("value", value);
             const articles = response.articles.slice(0, 5);
             const articlesRoot = $(".contrasting-article").first();
             for (let articleObj of articles) {
                 const article = $("<div class='article'></div>");
                 article.append(`<a class='article-header' href='${articleObj.url}'>${articleObj.title}</a>`);
-                // article.append(`<div class='bias-bar'><div class=bias${articleObj.bias}></div></div>`);
+            //     article.append(`<div class = "slidercontainer">
+            //     <input type="range" min="1" max="100" value="20" class="slider" id="source-slider">
+            // </div>`);
                 articlesRoot.append(article);
             }
         });
     })
 }
-
-$(function() {
-    $("#myRange").disabled = true;
-});
