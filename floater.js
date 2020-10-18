76518222bcf05194e05a1828f04842bf58c9ebe2
Ian Wang <35878784+iwangy@users.noreply.github.com>
2020-10-18 00:24:06 -0700

    more slider experimentation

--- floater.js
+++ floater.js
@@ -47,13 +47,6 @@ function populateFloater(floater, publisher) {
 }
 
 var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
 
 function setSlider() {
     document.getElementById("myRange").value = "75";
